7df8aeba4f9937161eaff1f72c6cb55ff91e8522
Gustavo <gu.alvesslv@gmail.com>
2024-03-29 20:44:24 -0300

    Adicionado tela Activiy Resultado •Valor IMC •Classificação

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -36,7 +37,11 @@ class MainActivity : AppCompatActivity() {
                 val alturaQ2 = altura * altura
                 val resultado = peso / alturaQ2
 
-                println("Ação do botão $resultado")
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra(KEY_RESULT_IMC, resultado)
+                startActivity(intent)
+
+                println("Teste gtgt Ação do botão $resultado")
             }
         }
     }
