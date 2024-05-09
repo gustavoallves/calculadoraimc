88e867ce3e6a8bb6b49f5019ecf72eb7d8936e0b
Gustavo <gu.alvesslv@gmail.com>
2024-05-09 19:12:30 -0300

    • Melhorando experiência do usuário • Botão de voltar

--- app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
@@ -1,7 +1,9 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.TextView
 
 const val KEY_RESULT_IMC = "ResultActivity.KEY_IMC"
@@ -30,5 +32,11 @@ class ResultActivity : AppCompatActivity() {
             "OBESIDADE GRAVE"
         }
         tvClassificacao.text = classificacao
+
+        val btnback = findViewById<ImageButton>(R.id.btn_back)
+        btnback.setOnClickListener(){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
